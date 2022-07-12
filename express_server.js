ae5bde09cfcd962d10080ba7165822b154c6a508
Bayan Safaee <bayan.safaee@gmail.com>
2022-07-12 08:37:57 -0700

    add second route to Render information about a single URL.

--- express_server.js
+++ express_server.js
@@ -9,13 +9,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// ROUTING
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// Displays our urls from the urlDatabase by using the urls_index template
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+// Displays short URL and long URL
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
